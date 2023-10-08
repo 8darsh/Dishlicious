914b6ab84072ec2c0eea4d2c141271fa90c2c785
Adarsh <86889416+8darsh@users.noreply.github.com>
2023-10-08 19:45:00 +0530

    UI completed

---
+++ Dishlicious/RecipesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RecipesViewController.swift
+//  Dishlicious
+//
+//  Created by Adarsh Singh on 08/10/23.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var ingredients: UILabel!
+    @IBOutlet var recipe: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // Set the scroll view's contentSize property.
+
+    }
+
+    
+
+
+
+}
--- Dishlicious/ViewController.swift
+++ Dishlicious/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var headerView: UIView!
+    
+    @IBOutlet var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.headerView.layer.cornerRadius = 25
+        self.searchBar.layer.cornerRadius = 15
+        
     }
 
-
+    @IBAction func filterBtnTapped(_ sender: UIButton) {
+    }
+    
 }
 
